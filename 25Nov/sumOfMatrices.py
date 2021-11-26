4c633800316edd59589d690901d639c282ed2f48
[Kavish Boyal] <38506513+universekavish@users.noreply.github.com>
2021-11-26 07:46:26 +0530

    25Nov Assignment

--- 25Nov/sumOfMatrices.py
+++ 25Nov/sumOfMatrices.py
@@ -6,6 +6,27 @@ if len(sys.argv) != 3 :
 
 fin = open('matrix1.txt', 'r')
 fin2 = open('matrix2.txt', 'r')
+matrix1 = []
+for line in fin.readlines() :
+    a = []
+    for num in line.split() :
+        a.append(int(num))
+    matrix1.append(a)
+print(matrix1)
 
-for line in readlines() :
-    
+matrix2 = []
+for line2 in fin2.readlines() :
+    a = []
+    for num in line.split() :
+        a.append(int(num))
+    matrix2.append(a)
+print(matrix2)
+
+resultMatrix = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
+
+if len(matrix1) == len(matrix2) :
+    for i in range(len(matrix1)) :
+        for j in range(len(matrix2)) :
+            resultMatrix[i][j] = matrix1[i][j] + matrix2[i][j]
+
+print('Sum of ', matrix1, ' and ', matrix2, ' is: ', resultMatrix)
