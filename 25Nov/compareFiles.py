9dd3d13ead85e88a6de5383823c287822f31a7fd
[Kavish Boyal] <38506513+universekavish@users.noreply.github.com>
2021-11-26 08:09:42 +0530

    25Nov Assignment

--- 25Nov/compareFiles.py
+++ 25Nov/compareFiles.py
@@ -7,13 +7,28 @@ if len(sys.argv) != 3 :
 fin = open('input.txt', 'r')
 fin2 = open('input2.txt', 'r')
 
+differentLine = []
 count = 1
 while True :
     line = fin.readline()
     line2 = fin2.readline()
 
-    if line != line2 :
-        print('Different at ', count, ' line.')
-    else :
-        print(count)
-    count = count + 1
+    for line in fin.readlines() :
+        for line2 in fin2.readlines() :
+            if line == line2 :
+                print('Same at : ', count)
+            else :
+                differentLine.append(count)
+    print('different at ', differentLine)
+    # i = 0; j = 0
+    # while i < len(line) and j < len(line2) :
+    #     if line[i] != line2[j] :
+    #         i = i + 1
+    #         j = j + 1
+    #         print('Im good')
+    #         differentLine.append(count)
+    #     else : 
+    #         i = i + 1
+    #         j = j + 1
+    #     count = count + 1
+print('Different at lines : ', differentLine)
