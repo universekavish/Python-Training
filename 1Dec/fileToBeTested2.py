15393edc1af75f51089a55e9fb2f4f5b5b67d3ab
[Kavish Boyal] <38506513+universekavish@users.noreply.github.com>
2021-12-02 08:25:40 +0530

    1Dec Work

---
+++ 1Dec/fileToBeTested2.py
@@ -0,0 +1,24 @@
+import random
+
+def add (x, y) :
+    return x + y
+
+for i in range (5) :
+    print(i)
+
+for k in range (5) :
+    print(k)
+
+sum = add(3, 4)
+
+i = 3; j = 5
+if i < j :
+    print('True')
+
+while i < 5 :
+    print(i)
+    i = i + 1
+
+while d < 5 :
+    print(d)
+    d = d + 1
--- 1Dec/myStaticAnalyzer.py
+++ 1Dec/myStaticAnalyzer.py
@@ -5,30 +5,30 @@ import sys
 
 if len(sys.argv) == 1 : print('Invalid input ')
 
-word = ''
+#word = ''
 d = {}
 def updateD(word) :
-	if word not in d : 
-		d[word] = 0
-	else :
-		d[word] = d[word] + 1
+    if word not in d : 
+        d[word] = 0
+    else :
+        d[word] = d[word] + 1
 		
 i = 1
 for i in sys.argv : 
-	fin = open(i, 'r')
-	for line in fin.readlines() :
-		words = line.split(' ')
-		for word in words : 
-			if word == 'import' :
-				updateD(word)
-			elif word == 'def' :
-				updateD(word)
-			elif word == 'for' :
-				updateD(word)
-			elif word == 'while' :
-				updateD(word)
-			elif word == 'if' :
-				updateD(word)
-			else :
-				break
-print(d)
+    fin = open(i, 'r')
+    for line in fin.readlines() :
+        words = line.split(' ')
+        for word in words : 
+            if word == 'import' :
+                updateD(word)
+            elif word == 'def' :
+                updateD(word)
+            elif word == 'for' :
+                updateD(word)
+            elif word == 'while' :
+                updateD(word)
+            elif word == 'if' :
+                updateD(word)
+            else :
+                continue
+    print(d)
