ccd7dfbc8802662de0e0fc20fe99bb3aae4c6e18
[Kavish Boyal] <38506513+universekavish@users.noreply.github.com>
2021-12-08 08:00:03 +0530

    Coding practice

---
+++ CodingPractice/iterBothValAndInd.py
@@ -0,0 +1,10 @@
+L = [10, 23, 53, 23, 35, 54]
+
+for x, y in enumerate(L) :
+    print("{0} : {1}".format(x, y))
+
+
+l = ['some', 'other', 'list']
+
+for x, y in enumerate(l) :
+    print('%s : %s' % (x, y))
